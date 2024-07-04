54937669b5bd017ab06caca58f5c116b5941dc02
Naveenkumar Palani <98529771+NaveenkumarBE@users.noreply.github.com>
2024-07-04 21:47:40 +0530

    Inital commit

---
+++ HTTPClient _Request/HTTPClient _Request/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+class Program
+{
+    static async Task Main()
+    {
+        //Define the URL
+        string url = "https://example.com";
+
+        //Define the bearer token
+        string accessToken = "YOUR ACCESS TOKEN";
+
+        //Define the request body(If needed)
+        var data = new
+        {
+            key = "value"
+        };
+
+        //Serialize the data to JSON
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //Create an HTTPClient Instance
+        var client = new HttpClient();
+
+        //Add the bearer token to the request header
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+
+        //Make the HTTP request
+        var response = await client.PostAsync(url,content);
+
+        //print the response status code and content
+        Console.WriteLine($"Response Status Code :{response.StatusCode}");
+        Console.WriteLine($"Response Content :{await response.Content.ReadAsStringAsync()}");
+
+    }
+}
